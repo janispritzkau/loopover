53fd340e9667c8dd49e772e229a90c4511105853
Janis Pritzkau <janispritzkau@gmail.com>
2019-01-18 22:55:49 +0100

    Change theme color

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicPath: "/",
   pwa: {
     name: "Loopover",
-    themeColor: "#2698a7",
+    themeColor: "#47587a",
     iconPaths: {
       favicon32: "favicon-32x32.png",
       favicon16: "favicon-16x16.png"
