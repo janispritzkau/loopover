6dbc629c902b0641d1ea1fd380056953f82dad71
Janis Pritzkau <janispritzkau@gmail.com>
2019-01-19 11:48:58 +0100

    Make the blue a bit less intense

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -106,7 +106,7 @@ export class Game {
                 const index = this.board.grid[(row + this.rows * 8) % this.rows][(col + this.cols * 8) % this.cols]
                 const cx = (index % this.cols + 0.1) / (this.cols - 0.6)
                 const cy = (((index / this.cols) | 0) + 0.2) / (this.rows - 0.6)
-                const color = [(1 - cx) * 230 + 20, cy * 190 + cx * (1 - cy) * 40 + 30, cx * 230]
+                const color = [(1 - cx) * 230 + 20, cy * 190 + cx * (1 - cy) * 50 + 30, cx * 220]
 
                 this.ctx.fillStyle = `rgb(${color.map(x => x | 0).join()})`
                 this.ctx.fillRect(x, y, this.tileSize, this.tileSize)
