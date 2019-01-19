7dea811496d085cca8fd7331fad874cae194b77d
Janis Pritzkau <janispritzkau@gmail.com>
2019-01-19 10:57:35 +0100

    Add Dialog component

---
+++ src/components/Dialog.vue
@@ -0,0 +1,60 @@
+<template>
+  <transition name="fade">
+    <div v-if="open" class="dialog-container" :class="{ open: open }" @click.self="close">
+      <div class="dialog">
+        <slot/>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Dialog extends Vue {
+  @Prop()
+  open = false
+
+  close() {
+    this.$emit("update:open", false)
+  }
+}
+</script>
+
+
+<style scoped>
+.dialog-container {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.25);
+  z-index: 100;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 150ms;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.dialog {
+  padding: 16px;
+  background: #f6f6f6;
+  min-width: 320px;
+  border-radius: 2px;
+  box-shadow: 0 14px 32px rgba(0, 0, 0, 0.2);
+  transition: transform 150ms ease-out;
+}
+
+.fade-enter .dialog, .fade-leave-to .dialog {
+  transform: scale(0.75);
+}
+</style>
