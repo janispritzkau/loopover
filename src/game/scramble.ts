a3a30851ba2aa8f11311ff22d470c1b3e5b6089d
Janis Pritzkau <janispritzkau@gmail.com>
2020-01-21 19:41:13 +0100

    Fix scramble algorithm for no regrips mode

--- src/game/scramble.ts
+++ src/game/scramble.ts
@@ -2,40 +2,42 @@ import { Board } from "./Board"
 import { Axis } from "."
 
 export function scrambleBoard(board: Board, noRegripsTile?: number) {
-  if (noRegripsTile != null) {
-    for (let i = 0; i < board.cols * board.rows + 32; i++) {
-      const axis = Math.random() > 0.5 ? Axis.Col : Axis.Row
-      const { row, col } = board.pos(noRegripsTile)!
-      const n = 1 + Math.floor(Math.random() * (axis == Axis.Col ? board.rows - 1 : board.cols - 1))
-      board.move({ axis, index: axis == Axis.Col ? col : row, n })
-    }
-  } else {
-    for (let tries = 0; tries < 20; tries++) {
+  for (let tries = 0; tries < 20; tries++) {
+    if (noRegripsTile != null) {
+      for (let i = 0; i < board.cols * board.rows + 50; i++) {
+        const axis = Math.random() > 0.5 ? Axis.Col : Axis.Row
+        const { row, col } = board.pos(noRegripsTile)!
+        const n = 1 + Math.floor(Math.random() * (axis == Axis.Col ? board.rows - 1 : board.cols - 1))
+        board.move({ axis, index: axis == Axis.Col ? col : row, n })
+      }
+    } else {
       randomizeBoard(board)
-      if (!board.isSolved() && isSolvable(board)) return
     }
+    if (!board.isSolved()) return
   }
 }
 
 function randomizeBoard(board: Board) {
+  if (board.cols < 2 || board.rows < 2) return
+
   const indices = [...Array(board.cols * board.rows)].map((_, i) => i)
   shuffle(indices)
+
+  if ((board.cols * board.rows) % 2 && countInversions(indices) % 2) {
+    [indices[0], indices[1]] = [indices[1], indices[0]]
+  }
+
   board.grid = board.grid.map(row => row.map(i => indices[i]))
 }
 
-function isSolvable(board: Board) {
-  if ((board.cols * board.rows) % 2 == 0) return true
-
-  const indices: number[] = []
-  board.grid.forEach(row => row.forEach(i => indices.push(i)))
-
+function countInversions(indices: number[]) {
   let inversions = 0
   for (let i = 0; i < indices.length; i++) {
     for (let j = i + 1; j < indices.length; j++) {
       if (indices[i] > indices[j]) inversions += 1
     }
   }
-  return inversions % 2 == 0
+  return inversions
 }
 
 function shuffle<T>(array: T[]) {
