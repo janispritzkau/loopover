a61b157751e69400e7eff52261af3ed86db967bd
Janis Pritzkau <janispritzkau@gmail.com>
2020-01-31 11:59:32 +0100

    Update vue config and change favicon link

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
   publicPath: ".",
   pwa: {
     name: process.env.VUE_APP_NAME,
-    themeColor: "#47587a"
+    themeColor: "#47587a",
+    appleMobileWebAppCapable: true,
+    manifestOptions: {
+      background_color: "#ffffff"
+    }
   }
 }
