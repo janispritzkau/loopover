0273969ef51c7bb604ec56526d6d0c6f4fc7b71c
Janis Pritzkau <janispritzkau@gmail.com>
2020-02-19 20:50:03 +0100

    Add watchers on next tick with immediate option

--- src/state/index.ts
+++ src/state/index.ts
@@ -21,21 +21,6 @@ export interface Solve {
 export const state = new State()
 export const vue = new Vue({ data: state })
 
-vue.$watch(() => [state.cols, state.rows, state.event, state.noRegrips], () => {
-  state.cols = Math.floor(Math.min(Math.max(state.cols, 1), 50))
-  state.rows = Math.floor(Math.min(Math.max(state.rows, 1), 50))
-
-  state.game.setBoardSize(state.cols, state.rows)
-  state.reset()
-})
-
-vue.$watch(() => [state.darkText, state.useLetters, state.wrapAround, state.transitionTime], () => {
-  state.game.darkText = state.darkText
-  state.game.useLetters = state.useLetters
-  state.game.wrapAround = state.wrapAround
-  state.game.transitionTime = state.transitionTime
-})
-
 openDatabase().then(async db => {
   if (!IDBObjectStore.prototype.getAll) return
   state.db = db
@@ -51,5 +36,22 @@ if (localStorage.loopover) {
 vue.$watch(() => saveSettings(state), settings => {
   localStorage.loopover = JSON.stringify(settings)
 }, { deep: true })
+
+vue.$nextTick(() => {
+  vue.$watch(() => [state.cols, state.rows, state.event, state.noRegrips], () => {
+    state.cols = Math.floor(Math.min(Math.max(state.cols, 1), 50))
+    state.rows = Math.floor(Math.min(Math.max(state.rows, 1), 50))
+
+    state.game.setBoardSize(state.cols, state.rows)
+    state.reset()
+  }, { immediate: true })
+
+  vue.$watch(() => [state.darkText, state.useLetters, state.wrapAround, state.transitionTime], () => {
+    state.game.darkText = state.darkText
+    state.game.useLetters = state.useLetters
+    state.game.wrapAround = state.wrapAround
+    state.game.transitionTime = state.transitionTime
+  }, { immediate: true })
+})
 
 export * from "./state"
