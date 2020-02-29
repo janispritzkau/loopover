1a55aa41ef297580c57bc9900d3532eeffadd2fb
Janis Pritzkau <janispritzkau@gmail.com>
2020-02-29 19:05:46 +0100

    Fix broken slider in firefox

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -60,7 +60,7 @@ export default class Slider extends Vue {
 
     this.active = true
     this.$el.focus()
-    const clientX = event instanceof TouchEvent ? event.touches[0].clientX : event.clientX
+    const clientX = event.type == "touchmove" ? (event as TouchEvent).touches[0].clientX : (event as MouseEvent).clientX
     this.setValue((clientX - this.$el.offsetLeft) / this.$el.offsetWidth * (this.max - this.min) + this.min)
 
     addEventListener("mousemove", this.handleTouchMove)
@@ -70,7 +70,7 @@ export default class Slider extends Vue {
   }
 
   handleTouchMove(event: TouchEvent | MouseEvent) {
-    const clientX = event instanceof TouchEvent ? event.touches[0].clientX : event.clientX
+    const clientX = event.type == "touchmove" ? (event as TouchEvent).touches[0].clientX : (event as MouseEvent).clientX
     this.setValue((clientX - this.$el.offsetLeft) / this.$el.offsetWidth * (this.max - this.min) + this.min)
   }
 
